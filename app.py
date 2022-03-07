c46be0c57158ad9738619163c9c63c78d8f97eef
Ivan <st.affeev@yandex.ru>
2022-03-07 11:39:14 +0300

    register and login form added

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import os
 from flask import Flask, request, make_response, session, abort
 from flask import render_template, redirect, jsonify
+from flask_login import LoginManager, logout_user, login_required, login_user
+from data.user import User
+
+from data import db_session
+from forms.userform import LoginForm, RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route("/")
@@ -11,10 +18,60 @@ def main_page():
     return render_template('main_content.html', title='TheLastWolfpack')
 
 
-def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Register',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Register',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User()
+        user.username = form.username.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(
+            User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Wrong login or password",
+                               form=form)
+    return render_template('login.html', title='Authorization', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
 
 
 if __name__ == '__main__':
-    main()
+    db_session.global_init('users.db')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
