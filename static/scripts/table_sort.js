3d47d362d1bf16b5414bed196c27df09a9e4906f
Ivan <st.affeev@yandex.ru>
2022-03-26 00:03:39 +0300

    sorting by boat type added

--- static/scripts/table_sort.js
+++ static/scripts/table_sort.js
@@ -5,6 +5,11 @@
         var dateParts = date.split(" ");
         return (dateParts[2] * 10000) + ($.inArray(dateParts[1], ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]) * 100) + (dateParts[0]*1);
     };
+
+    var customBoatNumToOrd = function (type) {
+        "use strict";
+        return parseInt(type.split(", ")[0].split("-")[1]);
+    };
      
     jQuery.fn.dataTableExt.aTypes.unshift(
         function (sData) {
@@ -27,6 +32,20 @@
         "use strict";
         var ordA = customDateDDMMMYYYYToOrd(a),
             ordB = customDateDDMMMYYYYToOrd(b);
+        return (ordA < ordB) ? 1 : ((ordA > ordB) ? -1 : 0);
+    };
+
+    jQuery.fn.dataTableExt.oSort['boat-num-asc'] = function (a, b) {
+        "use strict";
+        var ordA = customBoatNumToOrd(a),
+            ordB = customBoatNumToOrd(b);
+        return (ordA < ordB) ? -1 : ((ordA > ordB) ? 1 : 0);
+    };
+     
+    jQuery.fn.dataTableExt.oSort['boat-num-desc'] = function (a, b) {
+        "use strict";
+        var ordA = customBoatNumToOrd(a),
+            ordB = customBoatNumToOrd(b);
         return (ordA < ordB) ? 1 : ((ordA > ordB) ? -1 : 0);
     };
      
