377918f9f5ac018680d10a4e55fc7c1a602f3f82
SneakySelderey <sneakyselderey@yandex.ru>
2022-04-03 19:30:09 +0300

    bot logs mode set to INFO

--- discord_bot.py
+++ discord_bot.py
@@ -6,7 +6,7 @@ import requests
 from decouple import config
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
