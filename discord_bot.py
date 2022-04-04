2a2d3d9e6680b7228f99168a2435bb2eda0d347b
SneakySelderey <sneakyselderey@yandex.ru>
2022-04-04 22:10:21 +0300

    bot kinda plays music

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,5 @@
 from asyncio import tasks
+import asyncio
 import discord
 from discord.ext import commands, tasks
 import logging
@@ -6,6 +7,7 @@ import requests
 from decouple import config
 from random import choice
 from bs4 import BeautifulSoup
+import youtube_dl
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
@@ -16,6 +18,51 @@ logger.addHandler(handler)
 intents = discord.Intents.default()
 intents.members = True
 dashes = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, volume=0.5):
+        super().__init__(source, volume)
+
+        # self.data = data
+
+        # self.title = data.get('title')
+        # self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options, executable="static/sound/FFmpeg/bin/ffmpeg.exe"))
 
 
 class TheLastWolfpackAPI(commands.Cog):
@@ -101,6 +148,34 @@ class TheLastWolfpackAPI(commands.Cog):
         embedVar.add_field(name="3.", value="[Подводные лодки Кригсмарине](https://the-last-wolfpack.herokuapp.com/uboats)", inline=False)
         embedVar.add_field(name="4.", value="[Капитаны Кригсмарине](https://the-last-wolfpack.herokuapp.com/captains)", inline=False)
         await ctx.send(embed=embedVar)
+
+    @commands.command(name='join')
+    async def join(self, ctx, channel: discord.VoiceChannel):
+        """Joins a voice channel"""
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+        await channel.connect()
+
+    @commands.command(name='play_local')
+    async def play_local(self, ctx, query):
+        """Plays a file from the local filesystem"""
+
+        source = discord.FFmpegPCMAudio(query, **ffmpeg_options, executable="static/sound/FFmpeg/bin/ffmpeg.exe")
+        ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('Now playing: {}'.format(query))
+
+    @commands.command(name='play_yt')
+    async def play_yt(self, ctx, url):
+        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+        await ctx.send('Now playing!')
+        ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+    @commands.command(name='stop')
+    async def stop(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.disconnect()
 
     @commands.command(name='help')
     async def help(self, ctx):
