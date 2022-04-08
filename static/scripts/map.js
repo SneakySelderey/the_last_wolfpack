3f31d5b88146ab9daa978ba60aa50a1018d37587
Ivan <st.affeev@yandex.ru>
2022-04-08 22:15:22 +0300

    placemarks added

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,29 +1,34 @@
 let center = [50.97265436797006,9.793819812133787];
-let points = [];
 
 
-function getData() {
-   fetch('/api/uboats')
-  .then((response) => {
-      console.log('ok')
-      return response.json();
-  })
-  .then((data) => {
-      console.log(data);
-  });
+var getData = function(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'json';
+    request.onload = function() {
+        getCoords(request.response);
+    };
 
-}
+    request.send();
+};
+
 
 function getCoords(data) {
-    for(let i = 0; i < data.length; i++){
-        console.log(data[i]["coords"])
+    for(let i = 0; i < data["uboats"].length; i++){
+        let crds = data["uboats"][i]["coords"]
+        if (crds){
+            crds = [parseFloat(crds.split(', ')[1]),
+                    parseFloat(crds.split(', ')[0])]
+            points.push(crds);
+            json_data[crds] = data["uboats"][i]["tactical_number"]
+        }
     }
 }
 
 function init() {
 	let map = new ymaps.Map('map-test', {
 		center: center,
-		zoom: 17
+		zoom: 6
 	});
 
     map.controls.remove('geolocationControl'); // удаляем геолокацию
@@ -33,8 +38,15 @@ function init() {
     map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
     map.controls.remove('zoomControl'); // удаляем контрол зуммирования
     map.controls.remove('rulerControl'); // удаляем контрол правил
+    for(let i = 0; i < points.length; i++){
+        var placemark = new ymaps.Placemark(points[i], {}, {
+            preset: 'islands#redIcon'
+        });
+        map.geoObjects.add(placemark);
+    }
 }
 
+getData('/api/uboats');
 ymaps.ready(init);
-const boats = getData();
-console.log(boats);
+var points = [];
+var json_data = {};
